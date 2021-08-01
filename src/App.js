f7babf8a2c8a08b43ea52404786cfcfa585816ad
Ahmet Kelek <ahmet.kelek10@gmail.com>
2021-08-01 15:06:52 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
         <div className="row">
           <div className="col-3" style={{marginLeft:450}}>
             <Link to="" type="button" class="btn btn-primary btn-sm">
-              Ülkeler
+              Countries
             </Link>
           </div>
           <div className="col-3" style={{marginLeft:-100}}>
             <Link to="statistics" type="button" className="btn btn-primary btn-sm">
-              En çok Kullanılan Diller
+              Most Languages
             </Link>
           </div>
         </div>
